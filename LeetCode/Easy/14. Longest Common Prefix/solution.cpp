64aece95696f861a96186b2e7444a77abed75bcf
Salah Ashraf <salahashraf924@gmail.com>
2022-01-06 01:21:58 +0200

    Create solution.cpp

---
+++ LeetCode/Easy/14. Longest Common Prefix/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string sol="";
+        bool flag;
+        for(int i=0;i<strs[0].size();i++){
+            flag=true;
+            for(int j=1;j<strs.size();j++){
+                if(strs[0][i]!=strs[j][i] || i>=strs[j].size()){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)sol.push_back(strs[0][i]);
+            else break;
+        }
+        return sol;
+    }
+};
