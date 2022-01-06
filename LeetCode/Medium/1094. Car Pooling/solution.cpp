1c0cff2f0ae8aa1bbc54672e84c85de2a93d58d6
Salah Ashraf <salahashraf924@gmail.com>
2022-01-06 02:42:07 +0200

    RunTime : 12 ms & Memory usage : 9.7 MB

---
+++ LeetCode/Medium/1094. Car Pooling/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int stops[1001]={};
+        for(int i=0;i<trips.size();i++){
+            stops[trips[i][1]]+=trips[i][0];
+            stops[trips[i][2]]-=trips[i][0];
+        }
+        for(int i=0;i<1001 && capacity >=0;i++){
+            capacity-=stops[i];
+        }
+        return capacity >=0;
+    }
+};
