208046bd205c910943b38b8ada9d54e194e70df6
Salah Ashraf <salahashraf924@gmail.com>
2022-08-25 20:00:56 +0200

    write an optimized solution with low complexity

--- LeetCode/Easy/231. Power of Two/solution.cpp
+++ LeetCode/Easy/231. Power of Two/solution.cpp
@@ -13,3 +13,23 @@ public:
 
 // Runtime: 11 ms, faster than 5.57% of C++ online submissions for Power of Two.
 // Memory Usage: 6 MB, less than 23.44% of C++ online submissions for Power of Two.
+
+//another solution 
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)return false;
+        bool flag=true;
+        while(n){
+            if(n&1)
+                if(flag)
+                    flag=false;
+                else return false;
+            n/=2;
+        }
+        return true;
+    }
+};
+// Runtime: 3 ms, faster than 59.97% of C++ online submissions for Power of Two.
+// Memory Usage: 5.9 MB, less than 26.88% of C++ online submissions for Power of Two.
